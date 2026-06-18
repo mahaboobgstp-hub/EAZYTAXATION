943b03f745e0f600ad0c754fbde23f50087656b5
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 17:52:22 +0530

    Update companyService.js

--- src/services/companyService.js
+++ src/services/companyService.js
@@ -1,1 +1,28 @@
+import { supabase } from "../supabase/supabaseClient";
 
+export async function createCompany(data) {
+
+    const { error } = await supabase
+        .from("companies")
+        .insert([data]);
+
+    if (error) {
+        throw error;
+    }
+
+    return true;
+}
+
+export async function getCompanies() {
+
+    const { data, error } = await supabase
+        .from("companies")
+        .select("*")
+        .order("company_name");
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
