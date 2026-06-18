747c8c5fc7148bb958fac1ee566635f7bfb2587d
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:49:06 +0530

    Update and rename Suppliers.jsx to Vendors.jsx

---
+++ src/pages/masters/Vendors.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Vendors() {
+  return (
+    <div>
+      <h2>Vendors Master</h2>
+      <p>Vendor management module</p>
+    </div>
+  );
+}
+
+export default Vendors;
