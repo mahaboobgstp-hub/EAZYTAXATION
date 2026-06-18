eeba68be94f11b735bbdb73f457eecf81e1af9be
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:19:13 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,48 @@
+import { Link } from 'react-router-dom';
+import '../css/Sidebar.css';
 
+function Sidebar() {
+  return (
+    <div className="sidebar">
+
+      <h2 className="logo">
+        EAZYTAXATION
+      </h2>
+
+      <ul>
+
+        <li>
+          <Link to="/">Companies</Link>
+        </li>
+
+        <li>
+          <Link to="/customers">Customers</Link>
+        </li>
+
+        <li>
+          <Link to="/vendors">Vendors</Link>
+        </li>
+
+        <li>
+          <Link to="/sales">Sales</Link>
+        </li>
+
+        <li>
+          <Link to="/purchases">Purchases</Link>
+        </li>
+
+        <li>
+          <Link to="/expenses">Expenses</Link>
+        </li>
+
+        <li>
+          <Link to="/reports">Reports</Link>
+        </li>
+
+      </ul>
+
+    </div>
+  );
+}
+
+export default Sidebar;
