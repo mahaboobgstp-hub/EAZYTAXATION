d68f1b43967d79a192af63b5bc46bd72c3a7761c
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 22:50:39 +0530

    Create itemService.js

---
+++ src/services/itemService.js
@@ -0,0 +1,24 @@
+import { supabase } from "../supabase/supabaseClient";
+
+export async function createItem(data) {
+
+    const { error } = await supabase
+        .from("items")
+        .insert([data]);
+
+    if (error) throw error;
+
+    return true;
+}
+
+export async function getItems() {
+
+    const { data, error } = await supabase
+        .from("items")
+        .select("*")
+        .order("item_name");
+
+    if (error) throw error;
+
+    return data;
+}
