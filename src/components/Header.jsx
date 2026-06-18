210e878488350b59d03d788e58f0af9b62f7d379
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:19:36 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
+import '../css/Header.css';
 
+function Header() {
+  return (
+    <div className="header">
+
+      <h3>
+        EAZYTAXATION ERP
+      </h3>
+
+    </div>
+  );
+}
+
+export default Header;
