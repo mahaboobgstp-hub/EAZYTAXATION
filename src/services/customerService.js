8c0e328b66cbc197b99436d28fd193390cd779b6
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:45:28 +0530

    Update customerService.js

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,1 +1,24 @@
+import { supabase } from "../supabase/supabaseClient";
 
+export async function createCustomer(data) {
+
+    const { error } = await supabase
+        .from("customers")
+        .insert([data]);
+
+    if (error) throw error;
+
+    return true;
+}
+
+export async function getCustomers() {
+
+    const { data, error } = await supabase
+        .from("customers")
+        .select("*")
+        .order("customer_name");
+
+    if (error) throw error;
+
+    return data;
+}
