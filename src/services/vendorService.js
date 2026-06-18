bca105986695cf6f498e287cd65232c88751ea4a
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:46:14 +0530

    Update vendorService.js

--- src/services/vendorService.js
+++ src/services/vendorService.js
@@ -1,1 +1,24 @@
+import { supabase } from "../supabase/supabaseClient";
 
+export async function createVendor(data) {
+
+    const { error } = await supabase
+        .from("vendors")
+        .insert([data]);
+
+    if (error) throw error;
+
+    return true;
+}
+
+export async function getVendors() {
+
+    const { data, error } = await supabase
+        .from("vendors")
+        .select("*")
+        .order("vendor_name");
+
+    if (error) throw error;
+
+    return data;
+}
