958369359946600bbcd06632a3ab507d9e47ce0a
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-18 18:48:31 +0530

    Update Customers.jsx

--- src/pages/masters/Customers.jsx
+++ src/pages/masters/Customers.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Customers() {
+  return (
+    <div>
+      <h2>Customers Master</h2>
+      <p>Customer management module</p>
+    </div>
+  );
+}
+
+export default Customers;
