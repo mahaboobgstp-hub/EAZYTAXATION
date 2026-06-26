e95f9b4714f3d464ab70825ee11d9a8283c4afc6
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:34:54 +0530

    Update PrintHeader.jsx

--- src/components/print/PrintHeader.jsx
+++ src/components/print/PrintHeader.jsx
@@ -1,1 +1,38 @@
+import React from "react";
 
+function PrintHeader({
+  title,
+  subtitle = "",
+  leftContent = null,
+  rightContent = null,
+}) {
+  return (
+    <div className="print-header">
+
+      <div className="print-header-side">
+        {leftContent}
+      </div>
+
+      <div className="print-header-center">
+
+        <h1 className="print-document-title">
+          {title}
+        </h1>
+
+        {subtitle && (
+          <p className="print-document-subtitle">
+            {subtitle}
+          </p>
+        )}
+
+      </div>
+
+      <div className="print-header-side print-header-right">
+        {rightContent}
+      </div>
+
+    </div>
+  );
+}
+
+export default PrintHeader;
