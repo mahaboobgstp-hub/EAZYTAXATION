e7ec189923bf40494aaa802553bf0356ff5644a6
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:33:04 +0530

    Update PrintToolbar.jsx

--- src/components/print/PrintToolbar.jsx
+++ src/components/print/PrintToolbar.jsx
@@ -1,1 +1,56 @@
+import React from "react";
 
+function PrintToolbar({
+  onClose,
+  onPrint,
+  onDownload,
+  showClose = true,
+  showPrint = true,
+  showDownload = true,
+}) {
+  return (
+    <div className="print-toolbar">
+
+      <div className="print-toolbar-left">
+
+        {showClose && (
+          <button
+            type="button"
+            className="print-btn close-btn"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        )}
+
+      </div>
+
+      <div className="print-toolbar-right">
+
+        {showPrint && (
+          <button
+            type="button"
+            className="print-btn print-btn-primary"
+            onClick={onPrint}
+          >
+            Print
+          </button>
+        )}
+
+        {showDownload && (
+          <button
+            type="button"
+            className="print-btn download-btn"
+            onClick={onDownload}
+          >
+            Download PDF
+          </button>
+        )}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default PrintToolbar;
