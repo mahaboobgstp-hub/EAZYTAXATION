31e99d2ffa31df3f9a36bf26a3f3bd592a9dff6e
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 01:03:25 +0530

    Update PrintSignature.jsx

--- src/components/print/PrintSignature.jsx
+++ src/components/print/PrintSignature.jsx
@@ -1,1 +1,41 @@
+import React from "react";
 
+function PrintSignature({
+  show = true,
+  signatureUrl = "",
+  signatory = "Authorized Signatory",
+}) {
+
+  if (!show || !signatureUrl) {
+    return null;
+  }
+
+  return (
+
+    <div className="print-signature">
+
+      <div className="signature-image-wrapper">
+
+        <img
+          src={signatureUrl}
+          alt="Signature"
+          className="signature-image"
+        />
+
+      </div>
+
+      <div className="signature-line"></div>
+
+      <div className="signature-title">
+
+        {signatory}
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default PrintSignature;
