4bf7f57af967f500f40f57293008c048beca6b8e
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:37:26 +0530

    Update PrintInfo.jsx

--- src/components/print/PrintInfo.jsx
+++ src/components/print/PrintInfo.jsx
@@ -1,1 +1,72 @@
+import React from "react";
 
+function PrintInfo({
+  leftFields = [],
+  rightFields = [],
+}) {
+  return (
+    <div className="print-info">
+
+      {/* LEFT SIDE */}
+
+      <div className="print-info-left">
+
+        {leftFields.map((field, index) => (
+
+          <div
+            className="print-info-row"
+            key={index}
+          >
+
+            <div className="print-info-label">
+              {field.label}
+            </div>
+
+            <div className="print-info-colon">
+              :
+            </div>
+
+            <div className="print-info-value">
+              {field.value || "-"}
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
+
+      {/* RIGHT SIDE */}
+
+      <div className="print-info-right">
+
+        {rightFields.map((field, index) => (
+
+          <div
+            className="print-info-row"
+            key={index}
+          >
+
+            <div className="print-info-label">
+              {field.label}
+            </div>
+
+            <div className="print-info-colon">
+              :
+            </div>
+
+            <div className="print-info-value">
+              {field.value || "-"}
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default PrintInfo;
