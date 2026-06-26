e42209e558e231198b91b35ab1859a25a195d341
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:32:42 +0530

    Update PrintDocument.jsx

--- src/components/print/PrintDocument.jsx
+++ src/components/print/PrintDocument.jsx
@@ -1,1 +1,30 @@
+import React from "react";
+import "./Print.css";
 
+function PrintDocument({
+  toolbar,
+  children,
+  pageId = "print-document",
+  paper = "a4",
+}) {
+  return (
+    <div className="print-overlay">
+
+      {toolbar && (
+        <div className="print-toolbar-wrapper">
+          {toolbar}
+        </div>
+      )}
+
+      <div
+        id={pageId}
+        className={`print-document ${paper}`}
+      >
+        {children}
+      </div>
+
+    </div>
+  );
+}
+
+export default PrintDocument;
