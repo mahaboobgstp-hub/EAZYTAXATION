7b7ad8f80b8097832e110d4f61009664b1e2c7e7
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:43:04 +0530

    Update PrintTotals.jsx

--- src/components/print/PrintTotals.jsx
+++ src/components/print/PrintTotals.jsx
@@ -1,1 +1,73 @@
+import React from "react";
 
+function PrintTotals({
+  rows = [],
+  grandTotal = "",
+}) {
+
+  return (
+
+    <div className="print-totals">
+
+      <div className="print-totals-table">
+
+        {rows.map((row, index) => (
+
+          <div
+            className="print-total-row"
+            key={index}
+          >
+
+            <div className="print-total-label">
+
+              {row.label}
+
+            </div>
+
+            <div className="print-total-colon">
+
+              :
+
+            </div>
+
+            <div className="print-total-value">
+
+              {row.value}
+
+            </div>
+
+          </div>
+
+        ))}
+
+        <div className="print-grand-total">
+
+          <div>
+
+            Grand Total
+
+          </div>
+
+          <div>
+
+            :
+
+          </div>
+
+          <div>
+
+            {grandTotal}
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default PrintTotals;
