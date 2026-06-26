944c476c1ca69fcbb14e4123dd8e1992c84e0837
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-06-27 00:43:58 +0530

    Update PrintAmountWords.jsx

--- src/components/print/PrintAmountWords.jsx
+++ src/components/print/PrintAmountWords.jsx
@@ -1,1 +1,24 @@
+import React from "react";
 
+function PrintAmountWords({
+  amountInWords = "",
+  title = "Amount in Words",
+}) {
+  if (!amountInWords) return null;
+
+  return (
+    <div className="print-amount-words">
+
+      <div className="print-amount-title">
+        {title}
+      </div>
+
+      <div className="print-amount-text">
+        {amountInWords}
+      </div>
+
+    </div>
+  );
+}
+
+export default PrintAmountWords;
