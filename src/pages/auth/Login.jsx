5adcee27da7af2bf16d2e0ff559db40ebb5501f2
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-07-07 03:38:09 +0530

    Update Login.jsx

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../css/auth/Login.css";
-import { loginUser } from "../../services/authService";
+
 import {
 
     loginUser,
