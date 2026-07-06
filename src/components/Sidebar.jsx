23561586088817dc8a9de600de72a998e67a15ba
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-07-07 01:36:40 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -182,7 +182,7 @@ function Sidebar() {
 
                             <NavLink
 
-                                to="/"
+                                to="/companies"
 
                                 className="submenu-link"
 
