cb28a5e8609c741141d7adfb3e23bc9b30ab8a33
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-08-31 12:25:06 +0530

    Create Uom.jsx

---
+++ src/pages/masters/Uom.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Uom() {
+
+    return (
+
+        <div style={{ padding: "30px" }}>
+
+            <h2>Units (UOM) Master</h2>
+
+            <p>
+                Units of Measurement master page is working.
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Uom;
