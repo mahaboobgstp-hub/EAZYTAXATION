e818b59c7d9db828c08dd047f1d23d1d6acabc23
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-09-05 01:28:36 +0530

    Update AttendanceEntry.jsx

---
+++ src/pages/hr/AttendanceEntry.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AttendanceEntry() {
+    return (
+        <div style={{ padding: "25px" }}>
+            <h2>Attendance Entry</h2>
+            <p>Attendance module is under development.</p>
+        </div>
+    );
+}
+
+export default AttendanceEntry;
