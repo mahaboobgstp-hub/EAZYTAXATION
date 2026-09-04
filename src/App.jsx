6b48375e4a0b696137843a0e1a8a7ce4686a4edf
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-09-05 02:48:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import EmployeeMaster from "./pages/hr/EmployeeMaster";
 import AttendanceEntry from "./pages/hr/AttendanceEntry";
 import DepartmentMaster from "./pages/hr/DepartmentMaster";
 import DesignationMaster from "./pages/hr/DesignationMaster";
+import ShiftMaster from "./pages/hr/ShiftMaster";
 import ExpenseCategories from './pages/masters/ExpenseCategories';
 import AssetCategories from './pages/masters/AssetCategories';
 import SalesInvoice from './pages/sales/SalesInvoice';
@@ -180,7 +181,10 @@ function App() {
     path="designations"
     element={<DesignationMaster />}
 />
-
+<Route
+    path="shifts"
+    element={<ShiftMaster />}
+/>
         </Route>
         
       </Routes>
